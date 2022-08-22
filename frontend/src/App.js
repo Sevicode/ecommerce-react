e401a961cce18de66ec7df79ed3ee9a331912d13
Sevda <sevda.amini@gmail.com>
2022-08-22 20:46:22 +1000

    new components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import './index.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-     
-    </div>
+    <>
+     <Header />
+    </>
   );
 }
 
