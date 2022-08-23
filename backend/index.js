67909d7de830e3a4bd6dc2585ce8039ba9ac97af
Sevda <sevda.amini@gmail.com>
2022-08-23 21:58:59 +1000

    initial setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+const dotenv = require("dotenv")
+dotenv.config();
+
+const mongoose = require('mongoose');
+mongoose
+    .connect (process.env.MONGO_URL)
+    .then(() => console.log('DBConnection Successful!'))
+    .catch((err) => {console.log(err)
+    })
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
