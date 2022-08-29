27e17171993c102ae1fa9fdf794aba0f4a12cd60
Sevda <sevda.amini@gmail.com>
2022-08-29 12:54:56 +1000

    auth route created

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
+
 dotenv.config();
 
-const mongoose = require('mongoose');
+
 mongoose
     .connect (process.env.MONGO_URL)
     .then(() => console.log('DBConnection Successful!'))
     .catch((err) => {console.log(err)
     })
+app.use(express.json());
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,22 @@
 const router = require("express").Router();
+const User = require("../models/User")
+
+// Register:
+// Further work:  if there is not any body request/email/password throw 400 error and inside json we can say please send your email..
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    try{
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser)
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
 
 
 module.exports = router
